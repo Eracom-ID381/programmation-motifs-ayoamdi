3aeddc6b127ae7f387723315db99894f3c75bb56
ayoamdi <chaves.diana98@yahoo.com>
2020-04-08 15:25:53 +0200

    motif 1 part 2

--- 1/sketch.js
+++ 1/sketch.js
@@ -6,14 +6,38 @@ function setup() {
 }
 
 function draw() {
-    fill(0);
+    fill(0)
+    ellipse(width / 2, height / 2, 500);
 
+    fill(255);
+    ellipse(width / 2, height / 2 + 60, 210, 180)
+
+    fill(0)
+    ellipse(width / 2, height / 2 + 20, 220);
+
+    fill(255)
+    ellipse(width / 2 + 80, height / 2 - 50, 50);
+
+    fill(255)
+    ellipse(width / 2 - 80, height / 2 - 50, 50);
+
+    fill(0)
+    ellipse(width / 2 + 80, height / 2 - 70, 200, 50);
+
+    fill(0)
+    ellipse(width / 2 - 80, height / 2 - 70, 200, 50);
+
+
+
+    fill(0);
+    noStroke();
     for (let x = 10; x < width; x = x + 20) {
         for (let y = 10; y < height; y = y + 20) {
-            ellipse(x, y, 20);
+            ellipse(x, y, random(5, 25));
         }
 
     }
+
 
 }
 
